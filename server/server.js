05f04602604e9551bf13df95f1dd338f872ade34
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-05-19 12:34:00 +0300

    change icon, title.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { CLIENT_ID, CLIENT_SECRET } = process.env;
 
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const bodyParser = require('body-parser');
 
-const CLIENT_ID = '5b578c8c0d177a310fe7';
-const CLIENT_SECRET = 'e25835cb31ca4f523f3ede15004a2494ae40c6d7';
+// const CLIENT_ID = '5b578c8c0d177a310fe7';
+// const CLIENT_SECRET = 'e25835cb31ca4f523f3ede15004a2494ae40c6d7';
 
 const app = express();
 
@@ -39,24 +43,6 @@ app.get('/getAccessToken', async function (req, res) {
       res.json(data);
     });
 });
-
-// app.get('/getUserData', async function (req, res) {
-//   req.get('Authorization');
-
-//   await fetch('https://api.github.com/repos/facebook/react/issues', {
-//     method: 'GET',
-//     headers: {
-//       Authorization: req.get('Authorization'),
-//     },
-//   })
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(data => {
-//       console.log('GET', data);
-//       res.json(data);
-//     });
-// });
 
 app.listen(4000, function () {
   console.log('Cors server running on port 4000');
